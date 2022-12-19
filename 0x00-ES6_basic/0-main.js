f15ac2b7ef83fddaadbe9be08a8704572a97a64b
Forster112 <forstermichael112@gmail.com>
2022-12-19 22:57:53 +0100

    fixing

--- 0x00-ES6_basic/0-main.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable max-len */
-import { taskFirst, taskNext } from './0-constants.js';
-
-console.log(`${taskFirst()} ${taskNext()}`);
--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
 export default function createIteratorObject(report) {
-  const items = []
+  const items = [];
   for (const val of Object.keys(report.allEmployees)){
     for (const emp of report.allEmployees[val]){
         items.push(emp)
