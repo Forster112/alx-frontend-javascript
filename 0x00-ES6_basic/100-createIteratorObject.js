cf7d76ede46fd9beac0b9894d73468224e00d884
Forster112 <forstermichael112@gmail.com>
2022-12-19 23:00:13 +0100

    fixing

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const items = [];
   for (const val of Object.keys(report.allEmployees)){
     for (const emp of report.allEmployees[val]){
-        items.push(emp)
+      items.push(emp)
     }
   }
 
