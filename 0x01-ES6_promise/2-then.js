5a6f21a446f2235abe5234ac0c58945d88e6c34a
Forster112 <forstermichael112@gmail.com>
2022-12-20 20:56:13 +0100

    all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
-export default function handleResponseFromAPI(promise){
-  return new Promise((resolve, reject)=>{
-    if(promise){
-      resolve({status: 200, body: 'success'})
-    } else {
-      reject(new Error())
-    }
-  }).then(
-    console.log('Got a response from the API')
-  )
+export default function handleResponseFromAPI(promise) {
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
