467efef7ecade1092bac4f6052b50a401718883e
Forster112 <forstermichael112@gmail.com>
2022-12-23 02:33:01 +0100

    classes done

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,4 +1,7 @@
-import Currency from './3-currency.js';
+import EVCar from './100-evcar.js';
 
-const dollar = new Currency('$', 'Dollars');
-console.log(dollar.displayFullCurrency());
+const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
