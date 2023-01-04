d8afc8a4a3a696d0cacc8e80b0e4d4f96770320f
Forster112 <forstermichael112@gmail.com>
2023-01-04 22:36:35 +0100

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, str) {
+  let s = '';
   const arr = [];
   if (str && typeof str === 'string') {
     [...set].forEach((elem) => {
       if (elem.startsWith(str)) arr.push(elem.slice(str.length));
     });
+    s = arr.join('-');
   }
-  return arr.join('-');
+  return s;
 }
